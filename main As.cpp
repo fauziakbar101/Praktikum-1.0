ef56c97f542491f8cf9f2da307215ca0c496d3bb
fauziakbar101 <43899136+fauziakbar101@users.noreply.github.com>
2018-10-16 21:24:20 +0700

    Praktikum 1.0
    Percobaan

---
+++ main As.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+int p,l,luas;
+cout<<"Masukan Panjang Persegi = " ;cin>> p;cout<< endl;
+cout<<"Masukan Lebar Persegi = " ;cin>> l;cout<< endl;
+luas=p*l;
+cout<<" Jadi Luas Persegi Panjang Adalah = " <<luas;
+return 0;
+}
